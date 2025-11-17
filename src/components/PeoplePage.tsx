e1401a3875e4b1d5b7d4b9b4d6c39c4b0d9d9b26
Vladyslav Nechepurenko <fablevlados@gmail.com>
2025-11-17 18:39:47 +0200

    render PeopleFilter only while data is fetched

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -101,9 +101,11 @@ export const PeoplePage: React.FC = () => {
 
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
-          <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
-          </div>
+          {!loading && !error && people.length > 0 && (
+            <div className="column is-7-tablet is-narrow-desktop">
+              <PeopleFilters />
+            </div>
+          )}
 
           <div className="column">
             <div className="box table-container">
